0665e5a362aff7656086158bd944609d432d7d78
Rafaelszc <rafaelbjj84@gmail.com>
2025-11-12 22:40:27 -0300

    chore: hidden replay button when 'isRuning' is false

--- src/pages/Home/components/Buttons.jsx
+++ src/pages/Home/components/Buttons.jsx
@@ -9,7 +9,7 @@ export const Buttons = ({
     pomodogoSeries, setPomodogoSeries,
     isRuning, setIsRuning,
     startBark, setReset,
-    mode}) => {
+    theme}) => {
     const [onClose, setOnClose] = useState(true)
     const optionsProps = {
         setOnClose,
@@ -18,19 +18,21 @@ export const Buttons = ({
         minutesBreak,
         setMinutesBreak,
         pomodogoSeries,
-        setPomodogoSeries
+        setPomodogoSeries,
+        setReset,
+        theme
         }
 
     return (
         <div className="flex gap-4">
-            <button className="h-12 w-12 transition-colors duration-300 rounded-full flex justify-center items-center" onClick={() => {setIsRuning(!isRuning); if (!isRuning && pomodogoSeries-currentSerie > 1) startBark()}}>
-                <img className={`h-10 invert${mode === "dark" ? "" : "-0"}`} src={isRuning ? "/images/pause.png" : "/images/play.png"} alt="" />
+            <button className="h-12 w-12 transition-colors duration-300 flex justify-center items-center" onClick={() => {setIsRuning(!isRuning); if (!isRuning && pomodogoSeries-currentSerie > 1) startBark()}}>
+                <img className={`h-10 invert${theme === "dark" ? "" : "-0"}`} src={isRuning ? "/images/pause.png" : "/images/play.png"} alt="" />
             </button>
-            <button className="h-12 w-12 transition-colors duration-300 rounded-full flex justify-center items-center" onClick={() => setReset(true)}>
-                <span className={`w-7 h-7 bg-${mode === "dark" ? "white" : "black"}`} />
+            <button className={`${isRuning ? "flex" : "hidden"} h-12 w-12 transition-colors duration-300 justify-center items-center`} onClick={() => setReset(true)}>
+                <span className={`w-7 h-7 bg-${theme === "dark" ? "white" : "black"}`} />
             </button>
-            <button className={`${isRuning ? "hidden" : "flex"} h-12 w-12 rounded-full transition-colors duration-300 justify-center items-center`} onClick={() => setOnClose(false)}>
-                <img className={`h-10 invert${mode === "dark" ? "" : "-0"}`} src="/images/settings.png" alt="" />
+            <button className={`${isRuning ? "hidden" : "flex"} h-12 w-12 transition-colors duration-300 justify-center items-center`} onClick={() => setOnClose(false)}>
+                <img className={`h-10 invert${theme === "dark" ? "" : "-0"}`} src="/images/settings.png" alt="" />
             </button>
             
             {!onClose && <Options {...optionsProps} />}
